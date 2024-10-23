7b324dffe1b1b729790dfeca29f8e605844f4261
Tallen Pelissero <106496088+TallenPeli@users.noreply.github.com>
2024-10-23 09:46:03 -0400

    fixed the IP being set to localhost and making it so that you cannot access it from outside the machine

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -169,7 +169,7 @@ async fn get_weather() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(get_weather))
-        .bind(("127.0.0.1", 5000))?
+        .bind(("0.0.0.0", 5000))?
         .run()
         .await
 }
